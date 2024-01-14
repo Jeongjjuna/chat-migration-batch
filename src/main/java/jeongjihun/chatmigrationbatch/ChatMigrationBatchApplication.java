b1c588219fbe3d498120fefc1c336f81b0d6afd5
jeongjihun <wlgns2234@naver.com>
2024-01-14 11:56:14 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/jeongjihun/chatmigrationbatch/ChatMigrationBatchApplication.java
@@ -0,0 +1,13 @@
+package jeongjihun.chatmigrationbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMigrationBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMigrationBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jeongjihun/chatmigrationbatch/ChatMigrationBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jeongjihun.chatmigrationbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMigrationBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
